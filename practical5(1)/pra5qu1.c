a243e140a1021aa5d93e9f8eead9af7ab43eb9fb
tharuvithanage <140775911+tharuvithanage@users.noreply.github.com>
2023-07-30 19:25:43 +0530

    Add files via upload

---
+++ practical5(1)/pra5qu1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>                                                                                 
+int main()
+{
+	int x=1;
+	while(x<=100)
+	{
+		printf("%d ",x);
+		x++;
+	}
+}
+
