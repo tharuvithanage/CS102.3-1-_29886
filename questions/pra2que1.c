fa70462cd636136016089fedf8bc9ee2dcb70efa
tharuvithanage <140775911+tharuvithanage@users.noreply.github.com>
2023-07-30 10:04:13 +0530

    Add files via upload

---
+++ questions/pra2que1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+    int main ()
+   {
+	printf("HI, HOW OLD ARE YOU?\n \n");
+	
+	int age;
+	printf("enter the age");
+	scanf("%d",&age);
+	
+	printf("WELCOME %d\n",age);
+	printf("LET'S BE FRIENDS!");
+	
+  }
+
---
+++ questions/pra2que2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+    int main ()
+   {
+	printf("%5d%5d%5d\n \n", 2,4,6);
+               printf("%5d%5d%5d\n \n", 3,9,27);
+	printf("%5d%5d%5d\n \n", 4,16,64);
+	printf("\n\n\n");
+	
+	return 0;
+   }
+
