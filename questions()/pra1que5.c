f84437806a0e2a4a18f8a43ce3968d17b3920870
tharuvithanage <140775911+tharuvithanage@users.noreply.github.com>
2023-07-30 09:38:40 +0530

    Add files via upload

---
+++ questions()/pra1que5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+    int main ()
+    {
+	float n1,n2,average;
+               printf("Enter  number1: ");
+              printf("Enter  number2:");
+              average =(n1+n2)/2;
+	printf("%f",average);
+
+              return 0;
+     }
+
---
+++ questions()/que8.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+    int main()
+{
+    printf("The color: %s\n","blue" );
+    printf("1st number: %d\n",12345 );
+    printf("2nd number: %04d\n",25 );
+    printf("3rd number: %i\n",1234 );
+    printf("float number : %3.2f\n",3.14159);
+    printf("Hexadecimal number : %x\n",255);
+    printf("octal number : %o\n",255);
+    printf("Unsigned value : %u\n",150);
+    printf("Just print the percentage sign : %%\n",10);
+    return 0;
+}
+
